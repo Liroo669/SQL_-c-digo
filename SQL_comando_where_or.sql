f2e8f7b1c02f910f4af5bbc3191ed6e0ab66f44a
Murillo Albano Farias <murilloalbano71@gmail.com>
2026-03-11 22:07:43 -0300

    Add files via upload

---
+++ SQL_comando_where_or.sql
@@ -0,0 +1,7 @@
+SELECT * FROM DimProduct
+
+SELECT * FROM DimProduct
+WHERE BrandName = 'Contoso' OR ColorName = 'White'
+
+SELECT * FROM DimProduct
+WHERE BrandName = 'Contoso' OR BrandName = 'Fabrikam'
